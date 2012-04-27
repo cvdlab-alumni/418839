422df84f7a2f8dd7f17078cee8e01461230ec65a
Andrea Somma <andreasomma89@gmail.com>
2012-04-27 11:18:39 +0200

    esercizi in aula del 27 aprile

--- 2012-04-27/2012-04-27.js
+++ 2012-04-27/2012-04-27.js
@@ -1,6 +1,6 @@
 //ESERCIZI 27-04-2012
 
-//esercizio 1
+//ESERCIZIO 1
 function exercise1() {
 
   var points = [[0,0],[9,0],[9,-3],[8,-3],[8,-1],[5,-1],[5,-3],[4,-3],[4,-1],[1,-1],[1,-3],[0,-3],[0,0]];
@@ -18,14 +18,14 @@ function exercise1() {
 };
 //exercise1();
 
-//esercizio 2
+//ESERCIZIO 2
 function exercise2() {
   var roof = BOUNDARY(T([1,2])([-7,3])(CUBOID([9,7,0.5])));
   DRAW(COLOR([1,0,0,0.5])(roof));
 };
 //exercise2();
 
-//esercizio 3
+//ESERCIZIO 3
 function exercise3() {
   var domain = INTERVALS(1)(20);
   var controls = [[1,0],[1,1],[1,0],[1,1]];
@@ -34,7 +34,7 @@ function exercise3() {
 };
 //exercise3();
 
-//esercizio 4
+//ESERCIZIO 4
 function exercise4() {
   var domain = INTERVALS(1)(20);
   var controls = [[0,0],[1,2],[3,2],[3,0],[5,-1],[6,1]];
