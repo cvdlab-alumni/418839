956ace3f57a3a7d39d72621c73abb93e9553caa6
Andrea Somma <andreasomma89@gmail.com>
2012-05-05 12:53:09 +0200

    finito es2

--- 2012-05-04/exercise2.js
+++ 2012-05-04/exercise2.js
@@ -1,6 +1,7 @@
 function drawFuselage() {
   var domain1 = INTERVALS(1)(30);
   var domain2 = DOMAIN([[0,1],[0,1]])([20,30]);
+  var domain3 = DOMAIN([[0,1],[0,1],[0,1]])([20,10,5]);
 
   var p0 = [[0,0,0.2],[-0.8,5,0.4],[-0.4,6.2,0.5],[-0.3,6.5,0.5],[1.7,7,0.5],[0.2,3,0.3],[1.5,-0.5,0.3],[0,0,0.2]];
   var p1 = [[0,0,-0.2],[-0.8,5,-0.4],[-0.4,6.2,-0.5],[-0.3,6.5,-0.5],[1.7,7,-0.5],[0.2,3,-0.3],[1.5,-0.5,-0.3],[0,0,-0.2]];
@@ -47,8 +48,67 @@ function drawFuselage() {
   var cockpitStruct = STRUCT([cockpit, surface3, surface4]);
   cockpitStruct = T([0,1])([0.1,-0.05])(cockpitStruct);
 
-  var struct = STRUCT([fuselage, surface1, surface2, cockpitStruct]);
-  DRAW(COLOR([229/255,229/255,229/255,1])(struct));
+
+
+  function drawCylinderPieno(r,h,n,m,p,color) {
+    r = r || 1;
+    h = h || 2;
+    n = n || 40;
+    m = m || 1;
+    p = p || 1;
+    color = color || [1,1,1,1];
+
+    var dominioSolido = SIMPLEX_GRID([ REPEAT(n)(2*PI/n), REPEAT(m)((r)/m), REPEAT(p)(h/p) ]);
+    var mappingSolido = function(pt) {
+    return [pt[1]*SIN(pt[0]), pt[1]*COS(pt[0]), pt[2]];
+    };
+
+    dominioSolido = MAP(mappingSolido)(dominioSolido);
+    //DRAW(dominioSolido);
+    COLOR(color)(dominioSolido);
+
+    return dominioSolido;
+  };
+  //QUì INIZIA L'ELICA
+
+  var cilindro1 = drawCylinderPieno(0.02,0.2,20,1,1);
+  cilindro1 = T([0,1])([0.5,5.8])(R([1,2])([PI/2])(cilindro1));
+
+  var p8 = [[-0.1,0,0],[0.25,0.1,0.01],[0.40,0.05,0.03],[0.5,0.12,0.04],[0.6,0.05,0.03],[0.75,0.1,0.01],[1.1,0,0]];
+  var knots8 = [0,0,0,1,2,3,4,5,5,5];
+  var c8 = NUBS(S0)(2)(knots8)(p8);
+
+  var p9 = [[-0.1,0,0],[0.25,-0.1,0.01],[0.40,-0.05,0.03],[0.5,-0.12,0.04],[0.6,-0.05,0.03],[0.75,-0.1,0.01],[1.1,0,0]];
+  var knots9 = [0,0,0,1,2,3,4,5,5,5];
+  var c9 = NUBS(S0)(2)(knots9)(p9);
+
+  var p10 = [[-0.1,0,0],[0.25,0.1,-0.01],[0.40,0.05,-0.03],[0.5,0.12,-0.04],[0.6,0.05,-0.03],[0.75,0.1,-0.01],[1.1,0,0]];
+  var knots10 = [0,0,0,1,2,3,4,5,5,5];
+  var c10 = NUBS(S0)(2)(knots10)(p10);
+  
+  var p11 = [[-0.1,0,0],[0.25,-0.1,-0.01],[0.40,-0.05,-0.03],[0.5,-0.12,-0.04],[0.6,-0.05,-0.03],[0.75,-0.1,-0.01],[1.1,0,0]];
+  var knots9 = [0,0,0,1,2,3,4,5,5,5];
+  var knots11 = [0,0,0,1,2,3,4,5,5,5];
+  var c11 = NUBS(S0)(2)(knots11)(p11);
+
+  var curve5 = BEZIER(S1)([c8,c9]);
+  var surface5 = MAP(curve5)(domain2);
+
+  var curve6 = BEZIER(S1)([c10,c11]);
+  var surface6 = MAP(curve6)(domain2);
+
+  var surface56 = BEZIER(S2)([curve5,curve6]);
+  var solid56 = MAP(surface56)(domain3);
+  solid56 = T([1])([5.8])(R([1,2])([PI/2])(solid56));
+
+  var cilindro2 = drawCylinderPieno(0.05,0.01,20,1,1);
+  cilindro2 = T([0,1])([0.5,5.84])(R([1,2])([PI/2])(cilindro2));
+
+  var struct = STRUCT([fuselage, surface1, surface2, cockpitStruct, cilindro1]);
+  COLOR([229/255,229/255,229/255,1])(struct);
+  solid56 = COLOR([139/255,69/255,19/255,1])(solid56);
+  struct = STRUCT([struct,solid56,cilindro2]);
+  DRAW(struct);
 }
 
 drawFuselage();
